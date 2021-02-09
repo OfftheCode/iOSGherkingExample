8589b79d53c1a98fbf1241358fc311eeac87855a
OfftheCode <offthecode@gmail.com>
2021-02-09 01:20:35 +0100

    Working Gherkin

--- GherkinTestsTests/GherkinTestsTests.swift
+++ GherkinTestsTests/GherkinTestsTests.swift
@@ -6,28 +6,64 @@
 //
 
 import XCTest
+import XCTest_Gherkin
 @testable import GherkinTests
 
 class GherkinTestsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        self.continueAfterFailure = true
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testIsItFriday() {
+        Given("today is Sunday")
+        When("I ask wheter it's Friday yet")
+        Then("I should be told Nope")
     }
+    
+    func testFridayOutline() {
+        
+        Outline {
+            Given("today is <day>")
+            When("I ask wheter it's Friday yet")
+            Then("I should be told <answer>")
+        } _: { () -> [[String]] in
+            return [
+                ["day" , "answer"],
+                ["Friday", "Yes"],
+                ["Sunday", "Nope"],
+                ["Eeeeeee", "Nope"]
+            ]
+        }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+final class FridayServiceSteps: StepDefiner {
+    
+    private var today: String!
+    private var actualAnswer: String!
+    
+    private func isItFriday(_ day: String) -> String {
+        return day == "Friday" ? "Yes" : "Nope"
+    }
+    
+    override func defineSteps() {
+        
+        step("today is (.*)") { (day: String) in
+            self.today = day
+        }
+        
+        step("I ask wheter it's Friday yet") {
+            self.actualAnswer = self.isItFriday(self.today)
+        }
+        
+        step("I should be told (.*)") { (expectedAnswer: String) in
+            XCTAssertEqual(self.actualAnswer, expectedAnswer)
         }
     }
-
+    
+    
 }
---
+++ Pods/Target Support Files/Pods-GherkinTests-GherkinTestsUITests/Pods-GherkinTests-GherkinTestsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GherkinTests_GherkinTestsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GherkinTests_GherkinTestsUITests
+@end
